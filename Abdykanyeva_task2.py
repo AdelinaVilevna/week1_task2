cd9559d96682571b8e4471fcbd481fb656ac5560
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 22:55:40 +0600

    week1_task2

---
+++ Abdykanyeva_task2.py
@@ -0,0 +1,29 @@
+#Question 1
+
+# a = str("Hello")
+# b = str("world,")
+# c = str("I'm")
+# d = str("John!")
+# e = a + " " + b + " " + c + " " + d
+# print(e)
+
+
+#Question 2
+# n=input("How many times to multiply:")
+# print(" Hello" * int(n) )
+
+# 2-way
+# a_string = " Hello" * 3
+# print(a_string)
+
+
+# Question 3
+
+# string_name = input("What's your name:\n")
+# print("Hello," + string_name)
+
+
+# Question 4
+
+# word = "Hello"
+# print(word[::-1])
